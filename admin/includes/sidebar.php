51d7945dd6efa1e45fe6bf42e7d71e647041fa70
siopau <31457156+siopau@users.noreply.github.com>
2018-11-23 18:38:36 -0800

    Add files via upload

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,34 @@
+     <aside id="slide-out" class="side-nav white fixed">
+                <div class="side-nav-wrapper">
+                    <div class="sidebar-profile">
+                        <div class="sidebar-profile-image">
+                            <img src="../assets/images/profile-image.png" class="circle" alt="">
+                        </div>
+                        <div class="sidebar-profile-info">
+                       
+                                <p>Admin</p>
+
+                         
+                        </div>
+                    </div>
+            
+                <ul class="sidebar-menu collapsible collapsible-accordion" data-collapsible="accordion">
+                    <li class="no-padding"><a class="waves-effect waves-grey" href="dashboard.php">Dashboard</a></li>
+                   
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey">Employees</a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="addemployee.php">Add Employee</a></li>
+                                <li><a href="manageemployee.php">Manage Employee</a></li>
+       
+                            </ul>
+                        </div>
+                    </li>
+           
+    <li class="no-padding">
+        <a class="waves-effect waves-grey" href="logout.php">Sign Out</a>
+    </li>  
+</ul>
+</div>
+</aside>
